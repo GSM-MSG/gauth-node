adb09cd5abf6ec03b91900bd11e936997440013f
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-15 13:36:55 +0900

    feat: begin project

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import typescript from 'rollup-plugin-typescript2'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+  ],
+  plugins: [
+    typescript({
+      useTsconfigDeclarationDir: true,
+      tsconfig: './tsconfig.json',
+    }),
+    commonjs(),
+    nodeResolve(),
+    json(),
+  ],
+}
