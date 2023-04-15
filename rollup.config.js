6ba4ed937550cdf14ff3dd449edf274fced96675
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-15 16:17:12 +0900

    chore: fix rollup dts file

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import typescript from 'rollup-plugin-typescript2'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
+import { babel } from '@rollup/plugin-babel'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 export default {
   input: 'src/index.ts',
@@ -12,6 +14,7 @@ export default {
     },
   ],
   plugins: [
+    peerDepsExternal(),
     typescript({
       useTsconfigDeclarationDir: true,
       tsconfig: './tsconfig.json',
@@ -19,5 +22,11 @@ export default {
     commonjs(),
     nodeResolve(),
     json(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**/*.(ts|tsx|js|jsx)',
+      include: 'src/**/*.(ts|tsx|js|jsx)',
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.es', '.es6', '.mjs'],
+    }),
   ],
 }
