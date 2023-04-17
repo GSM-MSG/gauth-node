6622930c1c37c8ff11b825fc86283415d14d761f
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-17 09:29:26 +0900

    fix: absolute path

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,18 @@ import {
   issueCode,
   IssueCodeRequest,
   IssueCodeResponse,
-} from '@/services/issueCode'
+} from './services/issueCode'
 import {
   issueToken,
   IssueTokenRequest,
   IssueTokenResponse,
-} from '@/services/issueToken'
+} from './services/issueToken'
 import {
   reissueToken,
   ReIssueTokenRequest,
   ReIssueTokenResponse,
-} from '@/services/reissueToken'
-import { getUser, GetUserRequest, GetUserResponse } from '@/services/getUser'
+} from './services/reissueToken'
+import { getUser, GetUserRequest, GetUserResponse } from './services/getUser'
 
 const Gauth = {
   issueCode,
